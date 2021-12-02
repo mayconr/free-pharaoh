76398ea786e8f7cf8c65b72faf7f56af2a94d11a
mayconr <maycon.greg@gmail.com>
2021-12-02 10:44:26 -0300

    Initial commit

---
+++ desktop/src/com/github/freepharaoh/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.freepharaoh.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.freepharaoh.FreePharaohMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FreePharaohMain(), config);
+	}
+}
